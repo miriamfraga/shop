d63bda8c839b0f8b69a1248a8545c735cecc941a
“miriamfraga” <fragamr@outlook.es>
2023-02-16 13:11:31 +0100

    lazy 1

--- src/app/pages/shared/shared-routing.module.ts
+++ src/app/pages/shared/shared-routing.module.ts
@@ -1,13 +1,13 @@
 import { ProductsComponent } from './../products/products.component';
 import { NotfoundComponent } from './../notfound/notfound.component';
 import { ManageComponent } from './../manage/manage.component';
-import { HomeComponent } from './../home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from "@angular/core";
+import { HomeComponent } from '../home/home.component';
 
 
 const routes: Routes = [
-    {path: "home", component: HomeComponent},
+     {path: "home", component: HomeComponent},
     {path: "gestion", component: ManageComponent},
     {path: "notFound", component: NotfoundComponent},
     {path: "products", component: ProductsComponent}
