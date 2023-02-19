2268f7ac5baf3fd67aa01c4949e7ce847b65da12
“miriamfraga” <fragamr@outlook.es>
2023-02-19 20:46:13 +0100

    section illustration without styles

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,28 +1,113 @@
 import { ProductService } from './../../service/product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Item } from 'src/app/models/models';
+import { Iarray } from 'src/app/models/interface';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit{
+export class ProductsComponent {
   productList: Item[] = [];
   filteredList: any;
   filter!: string;
 
-  constructor(private ProductService: ProductService) {
-    this.filteredList= this.productList;
-    
-  }
+  constructor(private ProductService: ProductService) {}
+   public topProducts: Iarray = {
+   
+    category: "illustrations",
+    info: [
+    {
+     name: "Illustration Japanese",
+     description: "Illustation from ",
+     cares: "Ejbndh",
+     music: "Sjbndh",
+     price: 39.99,
+     image: {
+        src: "./assets/japanese.webp",
+        alt: "illustation"
+     }},
+     {
+      name: "Ficus Ginseng Kokedama",
+      description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aé",
+      cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agu",
+      music: "Soul/Jazz",
+      price: 39.99,
+      image: {
+         src: "./assets/japanese1.jpeg",
+         alt: "kokedama"
+      }},
+      {
+        name: "Ficus Ginseng Kokedama",
+        description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aé",
+        cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agu",
+        music: "Soul/Jazz",
+        price: 39.99,
+        image: {
+           src: "./assets/japanese2.jpeg",
+           alt: "kokedama"
+        }},
+        {
+       name: "Ficus Ginseng Kokedama",
+       description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aé",
+       cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agu",
+       music: "Soul/Jazz",
+       price: 39.99,
+       image: {
+          src: "./assets/japanese3.jpeg",
+          alt: "kokedama"
+       }},
+       {
+       name: "Ficus Ginseng Kokedama",
+       description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aé",
+       cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agu",
+       music: "Soul/Jazz",
+       price: 39.99,
+       image: {
+          src: "./assets/japanese4.jpeg",
+          alt: "kokedama"
+       }},
+     {
+       name: "Ficus Ginseng Kokedama",
+       description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aé",
+       cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agu",
+       music: "Soul/Jazz",
+       price: 39.99,
+       image: {
+          src: "./assets/japanese5.jpeg",
+          alt: "kokedama"
+       }},
+       {
+        name: "Ficus Ginseng Kokedama",
+        description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aé",
+        cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agu",
+        music: "Soul/Jazz",
+        price: 39.99,
+        image: {
+           src: "./assets/japanese6.jpeg",
+           alt: "kokedama"
+        }},
+        {
+         name: "FicKokedama",
+         description: "Una de ticulares raíces aé",
+         cares: "Estancia lumiequiere poca agu",
+         music: "Soul/Jazz",
+         price: 39.99,
+         image: {
+            src: "./assets/japanese7.jpeg",
+            alt: "kokedama"
+         }},
+         
+    ]
+    }
+  
+  
+
   ngOnInit(){
     this.ProductService.getProducts().subscribe((data:Item[])=>{
       this.productList = [...data];
     })
   }
-  // onFilterProduct(filter: string){
-    // const newList: any = this.productList.filter(pr => pr.name.includes(filter.toLowerCase()));
-    // this.filteredList = newList;
-  // }
+
 }
