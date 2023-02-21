897989408204d8068b005040cd9d0623b24fcab6
laurellx <laura.origa@gmail.com>
2023-02-21 13:28:45 +0100

    added id property and fixed input value error form

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,10 @@
+import { ProductRegistration } from './../models/productRegistration';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item } from '../models/models';
 
-const backUrl = "https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products"
+const backUrl : string = "https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products"
 
 @Injectable()
 export class ProductService {
@@ -11,6 +12,7 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   public productData = {
+    id: "",
     name: "",
     price: "",
     care: "",
