491ee522ff106db329a4ea1c320679b93c389c75
laurellx <laura.origa@gmail.com>
2023-02-21 17:55:53 +0100

    back added and fixed form

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -9,87 +9,94 @@ import { Iarray } from 'src/app/models/interface';
 })
 export class ProductComponent {
     
-    // bonsaisList: any[]= [];
+  bonsaisList: any[] = [];
     
-    //  constructor (private productServive:ProductService) {}
+     constructor (private productService:ProductService) {}
+
+     ngOnInit(): void {
+      this.productService.getBonsais().subscribe((data:any) => {
+        console.log(data);
+        this.bonsaisList = [...data]
+      })
+     } 
     //¿hace falta aquí pasar la función para obtener las pelis? No, no?
      
-    public arrayPlants: Iarray = {
-      category: "plants",
-      info: [
-      {
-        id: 1,
-       name: "Ficus Ginseng Kokedama",
-       description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aéreas hacen que quede perfecto con la kokedama",
-       cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agua",
-       music: "Soul/Jazz",
-       price: 39.99,
-       image: {
-          src: "./assets/kokedama.jpeg",
-          alt: "kokedama"
-       }
-    },
-      {
-        id: 2,
-        name: "Juniperus",
-        description: "Esta especie de bonsái es muy adecuada para diseños de madera muerta y se ve muy bien con sus finas almohadillas de agujas",
-        cares: "Exterior, sitio muy luminoso, abono orgánico, pinzar brotes largos para desarrollar nubes de follaje",
-        music: "Dodecaphonic",
-        price: 49.99,
-        image: {
-          src: "./assets/junipero.webp",
-          alt: "juniperus"
-       }
-    },
-      {
-        id: 3,
-        name: "Carmona Microphylla",
-        description: "Bonsai Carmona, también conocido como 'Árbol escorpión' por la forma de crecimiento de sus ramas, parecida a la figura de un escorpión",
-        cares: "Interior, temperaturas bajas, iluminación natural, regar con abundancia",
-        music: "Classic",
-        price: 39.99,
-        image: {
-          src: "./assets/carmona.avif",
-          alt: "Carmona"
-       }
-      },
-      { 
-        id: 4,
-        name: "Kotate Mochi",
-        description:"Tamañano entre 15 y 30 cm. Sencillo de mantener. Estilo bosque frondoso.",
-        cares: "Espacios abiertos y luminosos, poca agua. Evitar temperaturas extremas.",
-        music: "Gregorian",
-        price: 29.99,
-        image: {
-          src: "./assets/kotate.jpeg",
-          alt: "Kotate Mochi"
-       }
-    },
-    {  
-      id: 5,
-      name: "Empire Ficus",
-      description:"Tamañano entre 15 y 30 cm. Sencillo de mantener. Estilo bosque frondoso",
-      cares: "Espacios abiertos y luminosos, poca agua. Evitar temperaturas extremas.",
-      music: "Gregorian",
-      price: 29.99,
-      image: {
-        src: "./assets/empire.jpeg",
-        alt: "Empire"
-     }
-  },
-  { 
-    id: 6,
-    name: "Maple",
-    description:"Tamañano entre 15 y 30 cm. Sencillo de mantener. Estilo bosque frondoso",
-    cares: "Espacios abiertos y luminosos, poca agua. Evitar temperaturas extremas.",
-    music: "Gregorian",
-    price: 29.99,
-    image: {
-      src: "./assets/maple.jpeg",
-      alt: "Maple"
-   }
-}
-    ]
-    }
+//     public arrayPlants: Iarray = {
+//       category: "plants",
+//       info: [
+//       {
+//         id: 1,
+//        name: "Ficus Ginseng Kokedama",
+//        description: "Una de plantas más bonitas y elegantes. Su tronco y sus particulares raíces aéreas hacen que quede perfecto con la kokedama",
+//        cares: "Estancia luminosa, sol indirecto, evitar fuentes de aire directas, requiere poca agua",
+//        music: "Soul/Jazz",
+//        price: 39.99,
+//        image: {
+//           src: "./assets/kokedama.jpeg",
+//           alt: "kokedama"
+//        }
+//     },
+//       {
+//         id: 2,
+//         name: "Juniperus",
+//         description: "Esta especie de bonsái es muy adecuada para diseños de madera muerta y se ve muy bien con sus finas almohadillas de agujas",
+//         cares: "Exterior, sitio muy luminoso, abono orgánico, pinzar brotes largos para desarrollar nubes de follaje",
+//         music: "Dodecaphonic",
+//         price: 49.99,
+//         image: {
+//           src: "./assets/junipero.webp",
+//           alt: "juniperus"
+//        }
+//     },
+//       {
+//         id: 3,
+//         name: "Carmona Microphylla",
+//         description: "Bonsai Carmona, también conocido como 'Árbol escorpión' por la forma de crecimiento de sus ramas, parecida a la figura de un escorpión",
+//         cares: "Interior, temperaturas bajas, iluminación natural, regar con abundancia",
+//         music: "Classic",
+//         price: 39.99,
+//         image: {
+//           src: "./assets/carmona.avif",
+//           alt: "Carmona"
+//        }
+//       },
+//       { 
+//         id: 4,
+//         name: "Kotate Mochi",
+//         description:"Tamañano entre 15 y 30 cm. Sencillo de mantener. Estilo bosque frondoso.",
+//         cares: "Espacios abiertos y luminosos, poca agua. Evitar temperaturas extremas.",
+//         music: "Gregorian",
+//         price: 29.99,
+//         image: {
+//           src: "./assets/kotate.jpeg",
+//           alt: "Kotate Mochi"
+//        }
+//     },
+//     {  
+//       id: 5,
+//       name: "Empire Ficus",
+//       description:"Tamañano entre 15 y 30 cm. Sencillo de mantener. Estilo bosque frondoso",
+//       cares: "Espacios abiertos y luminosos, poca agua. Evitar temperaturas extremas.",
+//       music: "Gregorian",
+//       price: 29.99,
+//       image: {
+//         src: "./assets/empire.jpeg",
+//         alt: "Empire"
+//      }
+//   },
+//   { 
+//     id: 6,
+//     name: "Maple",
+//     description:"Tamañano entre 15 y 30 cm. Sencillo de mantener. Estilo bosque frondoso",
+//     cares: "Espacios abiertos y luminosos, poca agua. Evitar temperaturas extremas.",
+//     music: "Gregorian",
+//     price: 29.99,
+//     image: {
+//       src: "./assets/maple.jpeg",
+//       alt: "Maple"
+//    }
+// }
+//     ]
+//     }
  
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,31 +1,46 @@
+import { ProductRegistration } from './../models/productRegistration';
 import { Iarray } from 'src/app/models/interface';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item } from '../models/models';
 
-const backUrl = "https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products"
 
 @Injectable()
 export class ProductService {
 
+  backUrl: string = "https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products";
+  backBonsais: string = "http://localhost:3000/bonsais"
+
+
   constructor(private http: HttpClient) { }
 
   public productData = {
+    id: 0,
     name: "",
     price: "",
     care: "",
     music: "",
     description: "",
     image: "",
-    rating: ""
+    rating: "",
   }
 
 
   getProducts(): Observable <Item[]>{
-    return this.http.get<Item[]>(backUrl)
+    return this.http.get<Item[]>(this.backUrl)
   }
-  postProducts(product: Iarray){
-      return this.http.post(backUrl,product)
+  postProducts(product: ProductRegistration){
+      return this.http.post(this.backBonsais,product)
   }
+  getBonsais() {
+    return this.http.get(this.backBonsais)
+  }
+  getBonsai(id:number) {
+    return this.http.get(`${this.backBonsais}/${id}`)
+  }
+  deleteBonsai(id:number){
+    return this.http.delete(`${this.backBonsais}/${id}`)
+  }
+  
 }
