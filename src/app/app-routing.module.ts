fad04b78a7e17155cd0b3552ad82903c6308dc28
laurellx <laura.origa@gmail.com>
2023-02-25 00:51:51 +0100

    removed unused inports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,15 @@
 import { ProductsComponent } from './pages/products/products.component';
-import { ManageComponent } from './pages/manage/manage.component';
 import { MainComponent } from './core/main/main.component';
-import { ProductComponent } from './pages/product/product.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { DetailComponent } from './pages/detail/detail.component';
 
 const routes: Routes = [
        {path: "", loadChildren: ()=>
        import("./pages/shared/shared.module").then(m=>m.SharedModule)},
       {path: "", loadChildren: ()=>
        import("./pages/manage/manage.module").then(me=>me.ManageModule)},
-      // {path: "Manage", component:ManageComponent},
       {path: "Home", component: HomeComponent},
       {path: "Gallery", component: MainComponent},
       {path: "Products", component: ProductsComponent},
