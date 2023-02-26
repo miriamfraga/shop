64e6fde5be89834d7192c66982457ad8fc5df186
laurellx <laura.origa@gmail.com>
2023-02-26 20:33:52 +0100

    created input filter - still not working

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -11,25 +11,16 @@ export class ProductComponent {
   bonsaisList: any[] = [];
   filteredBonsai: any[] = [];
   isGrid: boolean = false;
+  
     
   constructor(private productService: ProductService) { }
 
-  filterBonsais(filter: any) {
-    this.filteredBonsai = this.bonsaisList.filter((bonsai) => {
-      bonsai.name.includes(filter);
-    })
-  }
-
   public selectGrid = () => {
     this.isGrid = true;
-    console.log(this.isGrid)
   }
-
   public selectList = () => {
     this.isGrid = false;
-    console.log(this.isGrid)
   }
-  
   
      ngOnInit(): void {
       this.productService.getBonsais().subscribe((data:any) => {
